5fdf7e99a62ff5dc0eba51ff7ba081a5acb3d701
mitesh129 <dev.mitesh.01@gmail.com>
2025-06-27 12:16:44 -0500

    Portfolio Index

--- Source/config.js
+++ Source/config.js
@@ -2,81 +2,97 @@ const portfolioItems = [
   {
     title: "Portfolio 1",
     image: "./Images/1.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_01/"
+    link: "https://findus-jdk-org.github.io/Resume_template_01/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_01.git",
   },
   {
     title: "Portfolio 2",
     image: "./Images/2.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_02/"
+    link: "https://findus-jdk-org.github.io/Resume_template_02/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_02.git",
   },
   {
     title: "Portfolio 3",
     image: "./Images/3.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_03/"
+    link: "https://findus-jdk-org.github.io/Resume_template_03/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_03.git",
   },
   {
     title: "Portfolio 4",
     image: "./Images/4.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_04/"
+    link: "https://findus-jdk-org.github.io/Resume_template_04/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_04.git",
   },
   {
     title: "Portfolio 5",
     image: "./Images/5.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_05/"
+    link: "https://findus-jdk-org.github.io/Resume_template_05/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_05.git",
   },
   {
     title: "Portfolio 6",
     image: "./Images/6.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_06/"
+    link: "https://findus-jdk-org.github.io/Resume_template_06/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_06.git",
   },
   {
     title: "Portfolio 7",
     image: "./Images/7.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_07/"
+    link: "https://findus-jdk-org.github.io/Resume_template_07/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_07.git",
   },
   {
     title: "Portfolio 8",
     image: "./Images/8.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_08/"
+    link: "https://findus-jdk-org.github.io/Resume_template_08/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_08.git",
   },
   {
     title: "Portfolio 9",
     image: "./Images/9.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_09/"
+    link: "https://findus-jdk-org.github.io/Resume_template_09/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_09.git",
   },
   {
     title: "Portfolio 10",
     image: "./Images/10.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_10/"
+    link: "https://findus-jdk-org.github.io/Resume_template_10/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_10.git",
   },
   {
     title: "Portfolio 11",
     image: "./Images/11.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_11/"
+    link: "https://findus-jdk-org.github.io/Resume_template_11/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_11.git",
   },
   {
     title: "Portfolio 12",
     image: "./Images/12.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_12/"
+    link: "https://findus-jdk-org.github.io/Resume_template_12/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_12.git",
   },
   {
     title: "Portfolio 13",
     image: "./Images/13.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_13/"
+    link: "https://findus-jdk-org.github.io/Resume_template_13/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_13.git",
   },
   {
     title: "Portfolio 14",
     image: "./Images/14.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_14/"
+    link: "https://findus-jdk-org.github.io/Resume_template_14/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_14.git",
   },
   {
     title: "Portfolio 15",
     image: "./Images/15.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_15/"
+    link: "https://findus-jdk-org.github.io/Resume_template_15/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_15.git",
   },
   {
     title: "Portfolio 16",
     image: "./Images/16.png",
-    link: "https://findus-jdk-org.github.io/Resume_template_16/"
+    link: "https://findus-jdk-org.github.io/Resume_template_16/",
+    gitlink: "https://github.com/findus-jdk-ORG/Resume_template_16.git",
   }
 ];
--- Source/script.js
+++ Source/script.js
@@ -7,7 +7,10 @@ portfolioItems.forEach((item) => {
     <img src="${item.image}" alt="${item.title} Thumbnail" class="thumbnail" />
     <div class='flex'>
         <h2>${item.title}</h2>
-        <a href="${item.link}" target="_blank" class="btn">Preview <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+        <div class='flex'>
+          <a href="${item.gitlink}" target="_blank" class="btn"><i class="fa-solid fa-code"></i></a>
+        <a href="${item.link}" target="_blank" class="btn"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+        </div>
     </div>
   `;
   container.appendChild(card);
